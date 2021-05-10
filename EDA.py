147356743c38de30f60068aa4d72b69c8522b939
Miiiingi <kimmingi722@gmail.com>
2021-05-10 20:37:00 +0900

    update

--- EDA.py
+++ EDA.py
@@ -19,7 +19,6 @@ def graph_binaryvariable(path) :
         axes[ind].set_title('{}'.format(variable))
     plt.show()
 # 1. graph binary variable 
-# graph_binaryvariable('train.csv')
 def inspect_birthday(path) :
     trainset = pd.read_csv(path)
     trainset['DAYS_BIRTH'] = (-trainset['DAYS_BIRTH'])//365
@@ -29,9 +28,25 @@ def inspect_birthday(path) :
     group_age_5 = trainset[(trainset['DAYS_BIRTH'] < 60) & (50 <= trainset['DAYS_BIRTH'])]
     group_age_6 = trainset[(trainset['DAYS_BIRTH'] < 70) & (60 <= trainset['DAYS_BIRTH'])]
     agelist = [[] for _ in range(5)]
-    for age in agelist : 
+    for ind, age in enumerate([group_age_2, group_age_3, group_age_4, group_age_5, group_age_6]) : 
         for credit in [x for x in range(3)] : 
-            print(sum(group_age_6['credit'] == credit)/len(group_age_6))
+            agelist[ind].append((sum(age['credit'] == credit)/len(age)))
+    return agelist
+def inspect_binary(path) : 
+    trainset = pd.read_csv(path)
+    variable_binary = trainset.columns[trainset.apply(pd.unique, axis=0).apply(len).apply(lambda x : x==2)]
+    list_value = [[] for _ in range(len(variable_binary))]
+    for ind, variable in enumerate(variable_binary) : 
+        for value in trainset[variable].unique() : 
+            for credit in range(3) : 
+                variable_value = (trainset[variable] == value)
+                variable_credit = (trainset['credit'] == credit)
+                list_value[ind].append((value,sum(variable_value & variable_credit)/sum(variable_value) ))
+    return list_value 
+# binary_graph = graph_binaryvariable('train.csv')
+# birthday = inspect_birthday('train.csv')
+binary_inspect = inspect_binary('train.csv')
+print(binary_inspect)
 
-inspect_birthday('train.csv')
+
 
